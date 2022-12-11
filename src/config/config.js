2fd0fbf5e4babb3b948f326c5bd7500ece0e4389
Reinaldo <reinaldoper83@gmail.com>
2022-12-11 14:30:54 -0300

    ajustes

--- src/config/config.js
+++ src/config/config.js
@@ -2,11 +2,13 @@
 
 require('dotenv').config();
 
+
 const config = {
   username: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
   dialect: 'mysql',
 };
 
